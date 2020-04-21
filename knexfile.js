3aeadf36422a1a8e2ee3713a3bfeca34288c69b0
JW Nicholson <john.nicholson812@gmail.com>
2020-04-21 15:14:50 -0400

    migration table added

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,22 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    useNullAsDefault:true,
+    client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
-      filename: './database/auth.db3'
+      filename: "./database/auth.db3"
+    },
     },
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations"
     },
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds"
     },
     pool: {
       afterCreate: (conn, done) => {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
-  },
-
+  }
 };
