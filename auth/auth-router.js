91137546ac68c62ea48280287083cecaa0e769be
JW Nicholson <john.nicholson812@gmail.com>
2020-04-21 15:49:01 -0400

    added post/login and generateToken() to auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,6 +24,39 @@ router.post('/register', (req,res) => {
         .catch(error => {
             res.status(500).json(error);
         });
+});
 
+router.post("/login", (req,res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = generateToken(user);
+
+                res.status(200).json({ token });
+            }else{
+                res.status(401).json({ message: "Invalid Credentials"});
+            }
+        })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+});
+
+function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        department: user.department
+    };
+
+    const options = {
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
